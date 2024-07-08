d6768df9e9ea5f6992d83566d57a704b4311441b
Karem_Mohamed <kareem.mohamed@everestminds.com>
2024-07-08 19:21:36 +0300

    test

--- routes/userRouter.js
+++ routes/userRouter.js
@@ -3,7 +3,7 @@ const userRouter = (router, myEmitter) => {
     console.log(process.env.NODE_ENV, "wwwwwwwwww");
     myEmitter.emit("fire", process.env.NODE_ENV);
     return res.json({
-      mess: `aws docker ci cd test api,${process.env.NODE_ENV},${process.env.PORT}`,
+      mess: `aws docker ci cd test api,${process.env.NODE_ENV},${process.env.PORT} try to test again`,
     });
   });
 
